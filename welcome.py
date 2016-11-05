899408ab116d1944a63c0c5884abe985d16c9a6c
TenKeyAngle <lidia.dokuchaeva@yahoo.com.au>
2016-11-05 21:54:58 +0800

    debugging Cloudant connection

--- welcome.py
+++ welcome.py
@@ -1,81 +1,36 @@
-# Copyright 2015 IBM Corp. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import os, json, cloudant
-from flask import Flask, jsonify
+import os
+import json
+import requests
+from flask import Flask
 
 app = Flask(__name__)
 
+app.config.update(
+    DEBUG=True,
+)
+
 @app.route('/')
-def Welcome():
-    return app.send_static_file('index.html')
+def welcome():
+    return 'Welcome to flask and Cloudant on Bluemix.'
 
-@app.route('/myapp')
-def WelcomeToMyapp():
-    return 'Welcome again to my app running on Bluemix!'
-
-@app.route('/api/people')
-def GetPeople():
-    list = [
-        {'name': 'John', 'age': 28},
-        {'name': 'Bill', 'val': 26}
-    ]
-    return jsonify(results=list)
-
-@app.route('/api/people/<name>')
-def SayHello(name):
-    message = {
-        'message': 'Hello ' + name
-    }
-    return jsonify(results=message)
-@app.route('/alchemy')
-def ConfirmConnection():
-    try:
-        key = os.environ.get('alchemyKey');
-    except:
-        return "Does not work..."
-    return "Alchemy is connected!"
-
-@app.route('/createdb')
+@app.route('/createdb/<db>')
 def create_db(db):
-   # try:
-      #  if 'VCAP_SERVICES' in os.environ:
-   #except:
-   #   return 'A Cloudant service is not bound to the application.  Please bind a Cloudant service and try again.'
-    db2info = json.loads(os.environ.get('VCAP_SERVICES'))['cloudantNoSQLDB'][0]
-    vcap = db2info['credentials']
-    cl_username = vcap['username']
-    cl_password = vcap['password']
-    #url         = vcap['url']
-    # auth        = ( cl_username, cl_password )
+    try:
+        vcap = json.loads(os.getenv("VCAP_SERVICES"))['cloudantNoSQLDB']
 
-   # in this case, https://garbados.cloudant.com
-    account = cloudant.Account(cl_username)
+        cl_username = vcap[0]['credentials']['username']
+        cl_password = vcap[0]['credentials']['password']
 
-    # login, so we can make changes
-    login = account.login(cl_username, cl_password)
-    # assert login.status_code == 200
+        url         = vcap[0]['credentials']['url']
+        auth        = ( cl_username, cl_password )
 
-    # create a database object
-    db = account.database('test')
+    except:
+        return 'A Cloudant service is not bound to the application.  Please bind a Cloudant service and try again.'
 
-    # now, create the database on the server
-    response = db.put()
-    print response.json()
-   # requests.put( url + '/' + db, auth=auth )
-   # return 'Database %s created.'
+    requests.put( url + '/' + db, auth=auth )
+    return 'Database %s created.' % db
 
-port = os.getenv('PORT', '5000')
+port = os.getenv('VCAP_APP_PORT', '5000')
+
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=int(port))
+    app.run(host='0.0.0.0', port=int(port))
