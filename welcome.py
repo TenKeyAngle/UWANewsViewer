3762ebb1d6fe70d58d2f03beb2265ac5dddbf99d
TenKeyAngle <lidia.dokuchaeva@yahoo.com.au>
2016-11-05 21:59:17 +0800

    debugging Cloudant connection

--- welcome.py
+++ welcome.py
@@ -1,17 +1,51 @@
-import os
-import json
-import requests
-from flask import Flask
+# Copyright 2015 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os, json
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-app.config.update(
-    DEBUG=True,
-)
-
 @app.route('/')
-def welcome():
-    return 'Welcome to flask and Cloudant on Bluemix.'
+def Welcome():
+    return app.send_static_file('index.html')
+
+@app.route('/myapp')
+def WelcomeToMyapp():
+    return 'Welcome again to my app running on Bluemix!'
+
+@app.route('/api/people')
+def GetPeople():
+    list = [
+        {'name': 'John', 'age': 28},
+        {'name': 'Bill', 'val': 26}
+    ]
+    return jsonify(results=list)
+
+@app.route('/api/people/<name>')
+def SayHello(name):
+    message = {
+        'message': 'Hello ' + name
+    }
+    return jsonify(results=message)
+@app.route('/alchemy')
+def ConfirmConnection():
+    try:
+        key = os.environ.get('alchemyKey');
+    except:
+        return "Does not work..."
+    return "Alchemy is connected!"
 
 @app.route('/createdb/<db>')
 def create_db(db):
@@ -30,7 +64,6 @@ def create_db(db):
     requests.put( url + '/' + db, auth=auth )
     return 'Database %s created.' % db
 
-port = os.getenv('VCAP_APP_PORT', '5000')
-
+port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(port))
+	app.run(host='0.0.0.0', port=int(port))
