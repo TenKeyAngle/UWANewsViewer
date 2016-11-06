9265805fb48695a8ccf5bae2b1c2019f1be421fa
TenKeyAngle <lidia.dokuchaeva@yahoo.com.au>
2016-11-07 00:44:00 +0800

    Debugging Cloudant module

--- welcome.py
+++ welcome.py
@@ -78,11 +78,6 @@ def testDB():
     try:
         client = Cloudant('1a818337-f029-449a-8a03-d34f30877d1d-bluemix',
                           'b20bcbf26bac5fa4ed56df09b07755ac1d8ccf6e3d3ad1177902957c1ca192c0', url='v')
-    except Exception as ex:
-        template = "An exception of type {0} occured. Arguments:\n{1!r}"
-        message = template.format(type(ex).__name__, ex.args)
-        return message
-    try:
         session = client.session()
     except Exception as ex:
         template = "An exception of type {0} occured. Arguments:\n{1!r}"
