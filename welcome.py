1fab7f31a5813997d21a305111e79afe8e60853e
TenKeyAngle <lidia.dokuchaeva@yahoo.com.au>
2016-11-07 01:38:57 +0800

    Trying view with Cloudant

--- welcome.py
+++ welcome.py
@@ -100,7 +100,7 @@ def testDB():
     #return jsonify(results=list)
     try:
         end_point = '{0}/{1}'.format(url, 'x/_design/des/_view/new-view?limit=200&reduce=false')
-        response = client.r_session.get(end_point, params=params)
+        response = client.r_session.get(end_point)
         return response.json()
     except Exception as ex:
         template = "An exception of type {0} occured. Arguments:\n{1!r}"
