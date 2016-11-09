6bccde1990a9b0d84fc0c7ca487a58b66f610dd5
TenKeyAngle <lidia.dokuchaeva@yahoo.com.au>
2016-11-09 22:48:58 +0800

    still trying to debug chart

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
 import json
+import pygal
+from pygal.style import BlueStyle
 import requests
 
 class LinkForm(FlaskForm):
@@ -25,9 +27,40 @@ r = requests.get(end_point)
 r = r.json()
 #print(r)
 t = []
-for item in r['rows']:
+for item in r.get('rows'):
     dict={}
     dict['key'] = item.get('key')
     dict['value'] = item.get('value')
     t.append(dict)
-print(t)
+#print(t)
+
+
+def testDB():
+    try:
+        end_point = '{0}/{1}'.format(cl_url, 'test/_design/des/_view/getrelevance')
+        r = requests.get(end_point)
+        r = r.json()
+        t = []
+        for item in r.get('rows'):
+            dict={}
+            dict['key'] = item.get('key')
+            dict['value'] = item.get('value')
+            t.append(dict)
+        relevance =  [float(i['value']) for i in t]
+        title = 'Most Relevant Topics'
+    except Exception as ex:
+        template = "An exception of type {0} occured. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        return "3: " + message
+    try:
+        bar_chart = pygal.Bar(title=title, style=BlueStyle)
+        bar_chart.x_labels = ['%s' % str(i['key']) for i in t]
+        bar_chart.add('Relevance', relevance)
+    except Exception as ex:
+        template = "An exception of type {0} occured. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        return "4: " + message
+    bar_chart.render_to_file('sample.svg')
+    return "success"
+
+print(testDB())
