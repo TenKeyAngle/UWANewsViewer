53d282d8b79d294ce4f5abe5c8db14fdcf1ddfd6
TenKeyAngle <lidia.dokuchaeva@yahoo.com.au>
2016-11-09 17:58:54 +0800

    continuing to test template

--- welcome.py
+++ welcome.py
@@ -99,7 +99,7 @@ def GetUrl():
         {'name': 'Bill', 'val': 26}
     ]
     #return jsonify(list)
-    return render_template('layout.html', message=jsonify(list.text))
+    return render_template('layout.html', message=jsonify(list))
 
 @app.route('/scrape')
 def Scrape():
