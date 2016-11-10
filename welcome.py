25670cc18ca4226156ded74c6b6f053af216cb95
TenKeyAngle <lidia.dokuchaeva@yahoo.com.au>
2016-11-10 22:10:26 +0800

    Trying form and also template

--- welcome.py
+++ welcome.py
@@ -67,14 +67,14 @@ except Exception as ex:
 
 @app.route('/')
 def Welcome():
-    # form = LinkForm(request.args)
-    form = LinkForm()
+    form = LinkForm(request.args)
+    #form = LinkForm()
     # if request.method == 'GET':
     #     if form.validate() == False:
     #         return render_template('index.html', form = form)
     #     else:
     #         return redirect(url_for('GetUrl', name=form.name.data))
-    return render_template('index.html', form-form)
+    return render_template('index.html', form=form)
 
 @app.route('/jsontest')
 def JsonTest():
