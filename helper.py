f56d430de39e2fa36e5a0140c7594bd9619f819d
TenKeyAngle <lidia.dokuchaeva@yahoo.com.au>
2016-11-10 22:42:03 +0800

    Trying form and also template

--- helper.py
+++ helper.py
@@ -12,7 +12,7 @@ import operator
 # Helper methods and classes for the main application
 
 class LinkForm(FlaskForm):
-    name = StringField('URL', validators=[validators.input_required()])
+    name = StringField('Search by News URL', validators=[validators.input_required()])
 
 # Function to determine how many AlchemyAPI calls are left
 # Gotten from https://gist.github.com/ianozsvald/4464247
