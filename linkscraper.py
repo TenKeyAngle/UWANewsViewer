b3426b57f6164e96108e7861c8b7c13f0d4f033c
TenKeyAngle <lidia.dokuchaeva@yahoo.com.au>
2016-11-11 21:27:30 +0800

    trying to use pipelines

--- linkscraper.py
+++ linkscraper.py
@@ -7,7 +7,7 @@ from scrapy.crawler import CrawlerProcess
 class NewsSpider(scrapy.Spider):
     custom_settings = {
         'ITEM_PIPELINES': {
-            '21331893.URLPipeline': 400
+            'URLPipeline': 400
         }
     }
     name = 'newsspider'
