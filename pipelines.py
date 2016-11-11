87b8481d98d4fcfbf1fd555e3b4e072bb652fc74
TenKeyAngle <lidia.dokuchaeva@yahoo.com.au>
2016-11-11 22:32:52 +0800

    testing pipeline

--- pipelines.py
+++ pipelines.py
@@ -24,17 +24,18 @@ class URLPipeline(object):
     def process_item(self, item, spider):
         combined_operations = ['title', 'authors', 'pub-date', 'entities', 'keywords',  'taxonomy', 'relations', 'concepts', 'doc-emotion']
         al = alchemy_calls_left(api_key=api_key)
-        if not al['consumedDailyTransactions'] < al['dailyTransactionLimit']:
+        #if not al['consumedDailyTransactions'] < al['dailyTransactionLimit']:
             # If limit surpassed, return a string letting user know
-            str = 'AlchemyAPI calls depleted for today: consumed.{}'.format(al['consumedDailyTransactions'])
-            raise CloseSpider(str)
+            #str = 'AlchemyAPI calls depleted for today: consumed.{}'.format(al['consumedDailyTransactions'])
+            # raise CloseSpider(str)
         if not item['url'] == None:
             # If item already in database, ignore it - if not, add analysis results to database
             if not item['url'] in self.t:
-                data = self.alchemy.combined(url=item, extract=combined_operations)
-                doc = self.database.create_document(data)
-                if not doc.exists():
-                    print("Doc not created: {0}".format(item['url']))
+                print("This should be happening for ones that do not exist")
+                # data = self.alchemy.combined(url=item, extract=combined_operations)
+                # doc = self.database.create_document(data)
+                # if not doc.exists():
+                #     print("Doc not created: {0}".format(item['url']))
             return item
         else:
             raise DropItem("Url is " % item['url'])
