26be7f41a4ad7ece62f109fe2b0eccedcaa63a95
TenKeyAngle <TenKeyAngle@users.noreply.github.com>
2016-11-11 23:40:16 +0800

    turning off debug

--- welcome.py
+++ welcome.py
@@ -328,4 +328,4 @@ def getHTML():
 # Run the app
 port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=int(port), debug=True)
+	app.run(host='0.0.0.0', port=int(port))
